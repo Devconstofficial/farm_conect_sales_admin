3537fe6f5b98308472fb2a5e634897b5f63b3812
Devconst-Account <usama@devconst.com>
2025-05-06 00:21:46 +0500

    gh-pages branch created

--- lib/utils/app_strings.dart
+++
@@ -1,7 +0,0 @@
-
-const String kDashboardScreenRoute = "/DashboardScreenRoute";
-const String kUserScreenRoute = "/UserScreenRoute";
-const String kEmployeeScreenRoute = "/EmployeeScreenRoute";
-const String kReportScreenRoute = "/ReportScreenRoute";
-const String kSalesScreenRoute = "/SalesScreenRoute";
-const String kChatScreenRoute = "/ChatScreenRoute";
