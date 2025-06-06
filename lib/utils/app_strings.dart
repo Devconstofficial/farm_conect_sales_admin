55ad83c8f9932ff51d6f42e5d9225b3814bec5cf
Devconst-Account <usama@devconst.com>
2025-06-06 15:13:43 +0500

    complete

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -153,13 +153,13 @@ const String kArabic = 'Arabic';
 
 
 // routs ===========
+const String kAuthScreenRoute = '/';
 const String kDashboardScreenRoute = "/DashboardScreenRoute";
 const String kUserScreenRoute = "/UserScreenRoute";
 const String kEmployeeScreenRoute = "/EmployeeScreenRoute";
 const String kReportScreenRoute = "/ReportScreenRoute";
 const String kSalesScreenRoute = "/SalesScreenRoute";
 const String kChatScreenRoute = "/ChatScreenRoute";
-const String kAuthScreenRoute = '/AuthScreenRoute';
 const String kSendOtpScreenRoute = '/SendOtpScreenRoute';
 const String kVerifyScreenRoute = '/VerifyScreenRoute';
 const String kSetNewPassScreenRoute = '/SetNewPassScreenRoute';
